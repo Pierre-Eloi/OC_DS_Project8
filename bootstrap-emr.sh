3e71e9ef87d34090ae191a5a0d3aa2c4c58a0046
Pierre-Eloi <pe.ragetly@gmail.com>
2023-01-28 12:57:36 +0100

    Add scipy and scikit-learn installation.

--- bootstrap-emr.sh
+++ bootstrap-emr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo python3 -m pip install -U pip setuptools
-sudo python3 -m pip install wheel pillow pandas pyarrow
+sudo python3 -m pip install wheel pillow scipy pandas pyarrow scikit-learn
 sudo python3 -m pip install tensorflow
